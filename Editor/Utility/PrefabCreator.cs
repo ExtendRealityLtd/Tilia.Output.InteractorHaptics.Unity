b766722b8ad03c83123d0b42c225594668b4bd54
extendreality <hello@extendreality.io>
2021-04-06 15:50:44 +0100

    feat(Utility): add prefab creator
    The latest version of Zinnia has the basis of a prefab creator that can
    be used to enable easy adding of prefabs to a scene without needing to
    drag and drop from directories. Instead a new menu item is added for
    quickly adding prefabs. The guide has been updated to accommodate this
    and the FodyWeavers.xml is now located in the root to serve both the
    Runtime and Editor scripts.

---
+++ Editor/Utility/PrefabCreator.cs
@@ -0,0 +1,28 @@
+namespace Tilia.Output.InteractorHaptics.Utility
+{
+    using System.IO;
+    using UnityEditor;
+    using Zinnia.Utility;
+
+    public class PrefabCreator : BasePrefabCreator
+    {
+        private const string group = "Tilia/";
+        private const string project = "Output/";
+        private const string menuItemRoot = topLevelMenuLocation + group + subLevelMenuLocation + project;
+
+        private const string package = "io.extendreality.tilia.output.interactorhaptics.unity";
+        private const string baseDirectory = "Runtime";
+        private const string prefabDirectory = "Prefabs";
+        private const string prefabSuffix = ".prefab";
+
+        private const string prefabInteractorHaptics = "Output.InteractorHaptics";
+
+        [MenuItem(menuItemRoot + prefabInteractorHaptics, false, priority)]
+        private static void AddInteractorHaptics()
+        {
+            string prefab = prefabInteractorHaptics + prefabSuffix;
+            string packageLocation = Path.Combine(packageRoot, package, baseDirectory, prefabDirectory, prefab);
+            CreatePrefab(packageLocation);
+        }
+    }
+}
